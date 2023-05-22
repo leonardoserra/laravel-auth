af627bf8a5abbaf96cba03af5b89ac619543c949
leonardoserra <serra.leonardo92@gmail.com>
2023-05-22 18:37:02 +0200

    creata rotta group che raggruppa admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware('auth','verified')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function (){
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+        Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
+
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
